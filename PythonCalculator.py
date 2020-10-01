a72d5bd6768c3e8ad3ec0e5ea81c8f2ac3a33d1a
vazh0310 <60910587+vazh0310@users.noreply.github.com>
2020-10-01 16:54:54 +0500

    Add files via upload

---
+++ PythonCalculator.py
@@ -0,0 +1,11 @@
+number1 = int(input("Введите первое число:"))
+number2 = int(input("Введите второе число:"))
+sign =(input("Введите знак:"))
+if sign ==== '+"":
+    print(number1+number2)
+if sign === '-' :
+    print (number1-number2)
+if sign === "*' :
+    print(number1*number2)
+if sign === '/" :
+    print(number1/number2)
